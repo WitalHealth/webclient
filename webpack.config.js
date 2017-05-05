ce9293eee4143b32983ee22c5394c8475374d5b0
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-05-05 12:33:16 +0200

    added dev server config for routes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry:  __dirname + '/client/index.js',
+  entry: __dirname + '/client/index.js',
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js',
@@ -10,6 +10,7 @@ module.exports = {
   },
   devServer: {
     inline: true,
+    historyApiFallback: true,
     port: 3000,
     contentBase: __dirname + '/build'
   },
@@ -20,30 +21,33 @@ module.exports = {
         exclude: /node_modules/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader', 'resolve-url-loader']
+          use: [ 'css-loader', 'sass-loader' ]
         })
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['babel-preset-react','babel-preset-es2015','babel-preset-stage-2']
-            }
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'babel-preset-react', 'babel-preset-es2015', 'babel-preset-stage-2' ]
+          }
         }
       },
       {
-        test: /\.(jpg|jpeg|png)$/i,
-        loader: 'file-loader'
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: './images/[name].[ext]',
+        },
       }
     ]
   },
   plugins: [
     new ExtractTextPlugin('style.css'),
-    new CopyWebpackPlugin([{
+    new CopyWebpackPlugin([ {
       from: 'index.html',
       to: 'index.html'
-    }])
+    } ])
   ]
 };
