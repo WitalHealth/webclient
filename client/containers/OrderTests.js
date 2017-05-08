3d4806429477916a90496e5818874ba8f0f5d1e8
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-05-08 11:49:42 +0200

    changed height of header and replaced remove icon for cart

--- client/containers/OrderTests.js
+++ client/containers/OrderTests.js
@@ -1,97 +1,112 @@
 import React, { Component } from 'react';
 import { get } from 'axios';
 import { DevFrame, DevTool } from '../components/DevTool';
-import classNames from 'classnames';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 import { StickyContainer, Sticky } from 'react-sticky';
+const removeIcon = require('../assets/images/error.png');
 
 class OrderTests extends Component {
   state = {
     tests: [],
     cart: [],
+    totalCount: null,
+    doAppearAnimation: true
   }
 
   componentWillMount() {
     get('https://dev.wital.se/api/test')
       .then(res => {
-        this.setState({ tests: res.data });
+        this.setState({ tests: res.data.slice(0, 20), totalCount: res.data.length });
       })
       .catch(err => console.error(err.message));
   }
 
   render() {
-    const { tests, cart } = this.state;
+    const { tests, cart, totalCount } = this.state;
 
     return (
-      <div className="page-container">
-        <h3>Antal: { tests.length }</h3>
-        <DevFrame>
-          <DevTool data={tests[ 0 ]} desc="Test model"/>
-        </DevFrame>
+      <div className="scroll-container" onWheel={() => this.handleFixed()}>
+        <div className="page-container">
+          <DevFrame>
+            <DevTool data={totalCount} desc="Total count"/>
+            <DevTool data={tests[ 0 ]} desc="Test model"/>
+          </DevFrame>
 
-        <div className="flex">
-          <div className="test-list">
-            <CSSTransitionGroup
-              transitionName="fade"
-              transitionEnterTimeout={400}
-              transitionLeaveTimeout={400}>
-              {
-                tests.map((test, i) =>
-                  this.notInCart(test) &&
-                  <div className="test" key={test.id} onClick={() => this.addToCart(test)}>
-                    <div className="desc">
-                      <h3>{test.name}</h3>
-                      <span>{test.description ? test.description : 'ingen beskrvning tillgänglig'} </span>
-                    </div>
-                    <div className="price"> {test.valueScript ? test.valueScript : '29:-'}</div>
-                    <button>Lägg till</button>
-                  </div>
-                )
-              }
-            </CSSTransitionGroup>
-
-          </div>
-          <StickyContainer>
-            <div className="side-bar">
-              <Sticky>
-                {
-                  ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
-                    return (
-                      <div className="cart" style={style}>
-                        <div className="cart-inner">
-                          <h2>Din beställning</h2>
-                          {
-                            !cart.length &&
-                            <div className="cart-item">
-                              Inga prover valda
-                            </div>
-                          }
-                          <CSSTransitionGroup
-                            transitionName="fade"
-                            transitionEnterTimeout={400}
-                            transitionLeaveTimeout={400}>
-                            {
-                              cart.length &&
-                              cart.map((cartItem, i) =>
-                                <div className="cart-item" key={cartItem.id}>
-                                  { cartItem.name}
-                                  <small className="remove" onClick={() => this.removeFromCart(cartItem)}>ta bort
-                                  </small>
-                                </div>
-                              )
-                            }
-                          </CSSTransitionGroup>
+          <div className="flex">
+            <div className="test-list">
+              <div className="test-inner">
+                <CSSTransitionGroup
+                  transitionName="test-fade"
+                  transitionEnterTimeout={400}
+                  transitionLeaveTimeout={400}
+                >
+                  {
+                    tests.map((test, i) =>
+                      this.notInCart(test) &&
+                      <div className="test" key={test.id} onClick={() => this.addToCart(test)}>
+                        <div className="test-inner">
+                          <div className="desc">
+                            <h3>{test.custName}</h3>
+                            <span>{test.description ? test.description : 'ingen beskrvning tillgänglig'} </span>
+                          </div>
+                          <div className="price"> {test.valueScript ? test.valueScript : '29:-'}</div>
+                          <button>Lägg till</button>
                         </div>
                       </div>
                     )
                   }
-                }
-              </Sticky>
+                </CSSTransitionGroup>
+              </div>
             </div>
-          </StickyContainer>
+            {/*<StickyContainer>*/}
+            <div id="side-bar" className="side-bar">
+              <div
+                className="cart"
+                ref={cart => this.cart = cart}
+                onMouseEnter={() => document.body.style.overflow = 'hidden'}
+                onMouseLeave={() => document.body.style.overflow = 'scroll'}
+              >
+                <h2>Din beställning</h2>
+                <div className="cart-inner">
+                  {
+                    !cart.length &&
+                    <div className="cart-item">
+                      Inga prover valda
+                    </div>
+                  }
+                  <CSSTransitionGroup
+                    transitionName="cart-fade"
+                    transitionEnterTimeout={400}
+                    transitionLeaveTimeout={400}>
+                    {
+                      cart.length &&
+                      cart.map((cartItem, i) =>
+                        <div className="cart-item" key={cartItem.id}>
+                          <div className="cart-item-inner">
+                            { cartItem.custName}
+                            <img className="icon remove" src={removeIcon} alt="ta bort"
+                                 onClick={() => this.removeFromCart(cartItem)}/>
+                          </div>
+                        </div>
+                      )
+                    }
+                  </CSSTransitionGroup>
+                </div>
+                <button className="btn">Gå vidare</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
+  }
+
+  handleFixed() {
+    if ( window.scrollY > 90 + 16 ) {
+      this.cart.style.position = 'fixed';
+    } else {
+      this.cart.style.position = 'static';
+    }
   }
 
   notInCart(test) {
@@ -100,8 +115,9 @@ class OrderTests extends Component {
   }
 
   addToCart(test) {
+    this.handleFixed();
     const { cart, tests } = this.state;
-    this.setState({ cart: [ ...cart, test ] });
+    this.setState({ cart: [ test, ...cart ] });
   }
 
   removeFromCart(test) {
