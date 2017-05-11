6690a4e63439e6f6e3a63da81e8c9216a269bf6a
lenalast <lenalast@gmail.com>
2017-05-11 10:26:34 +0200

    New build created

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -4,4 +4,8 @@ git add .
 git commit -m 'New build created'
 git push
 curl "https://dev.wital.se/api/clientdeploy?Sessionid=997"
+if curl "https://dev.wital.se/api/clientdeploy?Sessionid=997"
+then echo "Request was successful"
+else echo "CURL Failed"
 echo 'New build created!'
+fi
