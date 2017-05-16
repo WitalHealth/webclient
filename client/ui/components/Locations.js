5ac9a6d8b9db1bb4b62bc094b230b465d096176d
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-05-16 08:58:01 +0200

    fixed street name for locations

--- client/ui/components/Locations.js
+++ client/ui/components/Locations.js
@@ -8,7 +8,7 @@ class Locations extends Component {
 
   prettyStreet(street) {
     let n = street.indexOf(',');
-    return street.substring(0, n !== false ? n : street.length);
+    return street.substring(0, n !== -1 ? n : street.length);
   }
 
   componentWillMount() {
