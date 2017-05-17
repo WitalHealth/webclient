d9194e2f4544d1406abab603b72bae24fc7f67d2
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-05-17 09:31:21 +0200

    made locations responsive

--- client/ui/layouts/App.js
+++ client/ui/layouts/App.js
@@ -19,7 +19,7 @@ const App = () => (
   <Router>
     <div>
       <Route component={Header}/>
-      <Redirect to="/prover"/>
+      <Redirect to="/meddelanden"/>
       <Route exact path="/" component={Home}/>
       <Route path="/prover" component={Shop}/>
       <Route path="/provguiden" component={Guides}/>
--- client/ui/pages/Messages.js
+++ client/ui/pages/Messages.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import DefaultLayout from '../layouts/defaultLayout';
+
+const GridItem = () => (
+  <div className="grid-item">
+    <h1 className="city">Stadsnamn</h1>
+    <div className="name">Labbets namn</div>
+    <div className="street">gatuadress 1</div>
+  </div>
+);
 
 export default () => (
-  <div>
-    <h1>Meddelanden</h1>
-  </div>
+  <DefaultLayout>
+    <div id="grid">
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+     <GridItem/>
+    </div>
+  </DefaultLayout>
 );
