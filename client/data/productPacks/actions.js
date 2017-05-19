6be99741094935439c8f192df9925695a04d4b0e
lenalast <lenalast@gmail.com>
2017-05-19 10:26:46 +0200

    "provpaket" now shows 20 products

--- client/data/productPacks/actions.js
+++ client/data/productPacks/actions.js
@@ -4,7 +4,7 @@ export const fetchProductPacks = () => {
   return dispatch => {
     axios.get('https://dev.wital.se/api/product')
       .then(res => {
-        dispatch({ type: 'GET_PRODUCT_PACKS', productPacks: res.data, })
+        dispatch({ type: 'GET_PRODUCT_PACKS', productPacks: res.data.slice(0, 20), })
       })
       .catch(err => console.error(err.message));
   }
