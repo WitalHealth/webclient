636728f553426bf0c6214052688752af2fe1b887
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-06-02 10:45:41 +0200

    New build created

---
+++ webpack.prod.config.js
@@ -0,0 +1,47 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/client/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ 'css-loader', 'sass-loader' ]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'babel-preset-react', 'babel-preset-es2015', 'babel-preset-stage-2' ]
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: './images/[name].[ext]',
+        },
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new CopyWebpackPlugin([ {
+      from: 'client/index.html',
+      to: 'index.html'
+    } ])
+  ]
+};
