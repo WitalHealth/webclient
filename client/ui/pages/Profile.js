46c99c50c621aac788e9c44eb03561f26e11fef8
Stefan Bahnson <stefan.bahnson@findwise.com>
2017-06-02 11:18:43 +0200

    refactored profile to pure function component

--- client/ui/pages/Profile.js
+++ client/ui/pages/Profile.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import ProfileForm from '../components/ProfileForm.js';
-import { getActiveUser } from '../../data/user/actions';
 
-class Profile extends Component {
+const Profile = () => (
+  <div className="page-container">
+    <ProfileForm />
+  </div>
+);
 
-  componentDidMount() {
-    this.props.getActiveUser();
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log("lol");
-  }
-
-  render() {
-    return (
-      <div className="page-container">
-        <ProfileForm />
-      </div>
-    );
-  }
-}
-
-export default withRouter(
-  connect(
-    null,
-    { getActiveUser }
-  )(Profile));
+export default Profile;
